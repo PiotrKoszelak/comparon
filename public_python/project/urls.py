ba9db2a922d050afcdc35835d76194f8d116eabb
PK <koszelak.piotr@gmail.com>
2019-10-15 01:30:54 +0200

    Production

--- public_python/project/urls.py
+++ public_python/project/urls.py
@@ -1,22 +1,23 @@
 from django.contrib import admin
 from django.urls import path, re_path
-from app.views import OfferListCreate, OperatorListCreate, CityListCreate, PeriodListCreate, TypeListCreate, ContactListCreate, OfferDetailListCreate, ParametersListCreate
+from app.views import OfferListCreate, OperatorListCreate, CityListCreate, PeriodListCreate, TypeListCreate, Contact, OfferDetail, ParametersListCreate, SimpleOffer
 from frontend.views import main
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/offer/', OfferListCreate.as_view() ),
+    path('api/offers/', OfferListCreate.as_view() ),
     path('api/operator/', OperatorListCreate.as_view() ),
     path('api/city/', CityListCreate.as_view() ),
     path('api/period/', PeriodListCreate.as_view() ),
     path('api/type/', TypeListCreate.as_view() ),
     path('api/parameters/', ParametersListCreate.as_view() ),
-    path('api/contact/<int:pk>/', ContactListCreate.as_view() ),
-    path('api/offerdetail/<int:pk>/', OfferDetailListCreate.as_view() ),
+    path('api/contact/<int:pk>/', Contact.as_view() ),
+    path('api/offerdetail/<int:pk>/', OfferDetail.as_view() ),
+    path('api/offer/<int:pk>/', SimpleOffer.as_view() ),
     path('', main),
     path('about', main),
     path('contact', main),
     path('termsofuse', main),
     path('policyprivacy', main),
+    path('offers/compare', main),
 ]
