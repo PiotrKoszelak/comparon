0a1d0372cd5a7de1c0863585ba681c59cd8d147c
PK <koszelak.piotr@gmail.com>
2020-01-06 11:06:04 +0100

    add colors

---
+++ frontend/resources/src/style/colors.js
@@ -0,0 +1,5 @@
+export const primaryColor = '#efde25';
+export const primaryColorDark = '#b3a729';
+export const secondaryColor = '#898C8F';
+export const secondaryColorDark = '#374955';
+export const attentionColor = '#df0055';
