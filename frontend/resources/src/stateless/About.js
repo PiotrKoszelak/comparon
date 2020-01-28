7cedcc1cec91c316980085c0df1a7f7cff9b7f78
PK <koszelak.piotr@gmail.com>
2020-01-28 22:13:37 +0100

    about v2

--- frontend/resources/src/stateless/About.js
+++ frontend/resources/src/stateless/About.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles({
    root: {
+    zIndex: 2,
     position: 'relative',
     top: '30vh',
     height: '25vh',
@@ -69,10 +70,13 @@ const useStyles = makeStyles({
     },
   },
   picture: {
+    zIndex: 2,
     position: 'absolute',
+    opacity: 0,
     width: 'auto',
     height: 60,
     transition: 'transform 0.5s ease',
+    animation: '$show 1s linear 4s forwards',
     '&:hover' : {
       transform: 'scale(1.3, 1.3)',
       transition: 'transform 0.5s ease',
@@ -92,7 +96,39 @@ const useStyles = makeStyles({
       height: 60,
     },
   },
-
+  svgContainer: {
+    position: 'absolute',
+    width: '900px',
+    left: '40vw',
+    height: '100vh',
+    border: '2px solid blue',
+    top: -10,
+  },
+  svg: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
+  },
+  '@keyframes dash' : {
+    to : {
+      strokeDashoffset: 0,
+    }
+  },
+  '@keyframes show' : {
+    from : {
+      opacity: 0,
+    },
+    to : {
+      opacity: 1,
+    }
+  },
+  line: {
+    stroke: `${colors.secondaryColor}`, 
+    strokeWidth:2,
+    strokeDasharray: 300,
+    strokeDashoffset: 300,
+    animation: '$dash 4s linear forwards',
+  },
 });
 
 
@@ -115,24 +151,64 @@ function AboutContent ({language}){
               <div className={classes.button}>{translation.GO_TO_SEARCH[language]}</div>
           </Link>
       </div>
-      <div className={classes.container}>
-          <img className={classes.pictureMain} src={require(`../img/main.png`)} alt='compareON' />
-          <Picture label='test' name='1' left='40vw' top='75vh' />
-          <Picture label='test2' name='2' left='45vw' top='60vh' />
-          <Picture label='test3' name='3' left='35vw' top='45vh' />
-          <Picture label='test4' name='4' left='47vw' top='35vh' />
-          <Picture label='test5' name='5' left='52vw' top='15vh' />
-          <Picture label='test6' name='6' left='62vw' top='25vh' />
-          <Picture label='test7' name='7' left='75vw' top='13vh' />
-          <Picture label='test8' name='8' left='85vw' top='30vh' />
-          <Picture label='test9' name='9' left='90vw' top='50vh' />
-          <Picture label='test10' name='10' left='80vw' top='70vh' />
+
+      <div className={classes.svgContainer}>
+            <img className={classes.pictureMain} src={require(`../img/main.png`)} alt='compareON' />
+            <svg className={classes.svg} >
+              <line x1=" 270px" y1="calc(45vh + 150px)" x2="110px" y2="75vh" className={classes.line} />
+            </svg>
+            <Picture label='test' name='1' left='0' top='75vh'   />
+
+            <svg className={classes.svg} >
+              <line x1="270px" y1="calc(45vh + 75px)" x2="calc(75px + 70px)" y2="60vh" className={classes.line} />
+            </svg>
+            <Picture label='test2' name='2' left='75px' top='60vh'   />
+
+            <svg className={classes.svg} >
+              <line x1=" 270px" y1="calc(45vh + 75px)" x2="45px" y2="calc(45vh + 60px)" className={classes.line} />
+            </svg>
+            <Picture label='test3' name='3' left='-75px' top='45vh'   />
+
+            <svg className={classes.svg} >
+              <line x1=" 270px" y1="45vh" x2="225px" y2="calc(35vh + 60px)" className={classes.line} />
+            </svg>
+            <Picture label='test4' name='4' left='115px' top='35vh'   />
+
+            <svg className={classes.svg} >
+              <line x1=" 270px" y1="45vh" x2="235px" y2="calc(15vh + 60px)" className={classes.line} />
+            </svg>
+            <Picture label='test5' name='5' left='180px' top='15vh'   />
+
+            <svg className={classes.svg} >
+              <line x1="calc( 270px + 120px)" y1="45vh" x2="385px" y2="calc(25vh + 60px)" className={classes.line} />
+            </svg>
+            <Picture label='test6' name='6' left='330px' top='25vh'   />
+
+            <svg className={classes.svg} >
+              <line x1="calc( 270px + 240px)" y1="45vh" x2="580px" y2="calc(13vh + 60px)" className={classes.line} />
+            </svg>
+            <Picture label='test7' name='7' left='525px' top='13vh'   />
+
+            <svg className={classes.svg} >
+              <line x1="calc( 270px + 240px)" y1="45vh" x2="675px" y2="calc(30vh + 60px)" className={classes.line} />
+            </svg>
+            <Picture label='test8' name='8' left='675px' top='30vh'   />
+
+            <svg className={classes.svg} >
+              <line x1="calc( 270px + 240px)" y1="calc(45vh + 75px)" x2="750px" y2="calc(50vh + 30px)" className={classes.line} />
+            </svg>
+            <Picture label='test9' name='9' left='750px' top='50vh'   />
+
+            <svg className={classes.svg} >
+              <line x1="calc( 270px + 240px)" y1="calc(45vh + 150px)" x2="600px" y2="70vh" className={classes.line} />
+            </svg>
+            <Picture label='test10' name='10' left='600px' top='70vh'   />
       </div>
-    </div>
+  </div>
 )};
 
 
-function Picture ({label, name, left, top}){
+function Picture ({label, name, left, top, specClass}){
 
   const classes = useStyles();
   return(
