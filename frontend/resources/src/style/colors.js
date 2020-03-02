ebcb823de5c1b77791dd30f214be382ac03feaa0
PK <koszelak.piotr@gmail.com>
2020-03-02 19:39:47 +0100

    green color

--- frontend/resources/src/style/colors.js
+++ frontend/resources/src/style/colors.js
@@ -1,4 +1,5 @@
-export const primaryColor = '#efde25';
+//export const primaryColor = '#efde25';
+export const primaryColor = '#00b300';
 export const primaryColorDark = '#b3a729';
 export const secondaryColor = '#898C8F';
 export const secondaryColorDark = '#374955';
